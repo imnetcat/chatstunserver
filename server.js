6ef327c9680a31ef74145ed60035c9baddaaed3f
Гусь <37350375+imnetcat@users.noreply.github.com>
2019-04-17 11:27:44 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const WebSocket = require('ws');
  
 const wss = new WebSocket.Server({ port: 3000 });
  
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
+wss.on('connection', function connection(wss) {
+  wss.on('message', function incoming(message) {
     console.log('received: %s', message);
   });
  
-  ws.send('something');
+  wss.send('something');
 });
