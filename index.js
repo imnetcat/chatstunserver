bab33f10882399148d38b9cf08a40f4d27f92d39
Гусь <37350375+imnetcat@users.noreply.github.com>
2019-04-17 11:47:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
 'use strict';
-
-const WebSocket = require('ws');
-
-const wss = new WebSocket.Server({ port: 8080 });
- 
-wss.on('connection', function connection(wss) {
-  wss.on('message', function incoming(message) {
-    console.log('received: %s', message);
-  });
- 
-  wss.send('something');
-});
