3b6dc1649f4590296512d8c63dad2f9e1f3085c4
imnetcat <guskov.danil@gmail.com>
2019-10-19 18:34:51 +0000

    Переход на github.io

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var WebSocketServer = require("ws").Server;
 var http = require("http");
 var express = require("express");
 var app = express();
-var port = process.env.PORT || 5000;
+var port = 5000;
 
 app.use(express.static(__dirname + "/"));
 
